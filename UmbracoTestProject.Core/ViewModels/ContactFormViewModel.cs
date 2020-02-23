ebb0a10b94017e07b0df0d078c537a3ea084c140
Dejan Mihajlovik <dejan.mihajlovikj@axicos.com>
2020-02-23 17:55:47 +0100

    Task 4 - Contact page form

--- UmbracoTestProject.Core/ViewModels/ContactFormViewModel.cs
+++ UmbracoTestProject.Core/ViewModels/ContactFormViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace UmbracoTestProject.Core.ViewModels
+﻿using Our.Umbraco.DataAnnotations;
+
+namespace UmbracoTestProject.Core.ViewModels
 {
     public class ContactFormViewModel
     {
+        [UmbracoRequired]
         public string Email { get; set; }
 
+        [UmbracoRequired]
         public string Message { get; set; }
+
+        public string EmailTo { get; set; }
+        public string EmailSubject { get; set; }
     }
 }
